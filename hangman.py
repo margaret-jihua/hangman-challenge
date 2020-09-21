f2d16c9a5d0b9a07324777ee4f176de9b25c1b51
margare-jihua <margaret.jihua@gmail.com>
2020-09-20 23:35:45 -0700

    hangman game running

--- hangman.py
+++ hangman.py
@@ -1,9 +1,9 @@
 import random
 
 # define the bank of words
-words = ['amazing', 'awesome', 'snowwhite', 'chestnut', 'jedi', 'google']
+words = ['amazing', 'awesome', 'snowwhite', 'chestnut', 'jedi', 'google', 'banana', 'python', 'nutella']
 
-# store the drawing hangman in array
+# define the hangman drawing in array
 drawing = [
     " ____",
     "|    |",
@@ -14,31 +14,63 @@ drawing = [
     "-"
 ]
 
-# randomly pick a word to be the answer key
-key = random.choice(words)
-
-# display the current state of word guessed
+# define global variable
+key = ''
 display = []
-for i in range(0, len(key)):
-    display += '_'
-
-# try to see how hangman print out
-# for i in drawing:
-#     print(i)
-
-print(key, display) # TODO delete 
-
-#Game init
-print("~~~~~~~~~~~~~~Hangman Game~~~~~~~~~~~~~~")
-print("Guess the word or the man will be HANGED")
-print("You have 7 guesses\n", ' '.join(display))
-
-print("Hint: This word has {} letters ".format(len(key)))
-
-guess = input("Guess: ")
-if guess in key:
-    display[key.index(guess)] = guess
-    print(' '.join(display))
+hangman = []
 
 
-# while ()
+# initialize the game
+def game_init():
+    global key, display
+
+    # randomly pick a word to be the answer key
+    key = random.choice(words)
+
+    # display the current state of word guessed
+    for i in range(0, len(key)):
+        display += '_'
+
+
+# check if the letter user input is in the answer key
+def game_guess():
+    global key, display, hangman
+
+    guess = input("\nGuess: ")
+    if len(guess) != 1: # prevent multiple input
+        print("One Letter At A Time!")
+        game_guess()
+    else:
+        if guess in key:
+            display[key.index(guess)] = guess
+            # replace this letter in key with '#'
+            # if there are duplicate letters in the answer key, replace first one 
+            key = key.replace(guess, '#', 1)
+            print("Success revealed one letter '{}'✅".format(guess))
+        else:
+            hangman.insert(0, drawing.pop())
+            print("Wrong Guess❌")
+
+
+def game_run():
+    game_init()
+    print(key) # TODO delete 
+    print("~~~~~~~~~~~~~~Hangman Game~~~~~~~~~~~~~~")
+    print("Guess the word or the man will be HANGED")
+    print("You can have up to 7 wrong guesses\n")
+    print("Hint: This word has {} letters ".format(len(key)), ' '.join(display))
+    answer = key
+    while answer != ''.join(display):
+        game_guess()
+        print("👀 Current Guesses: {}".format(' '.join(display)))
+        if hangman != []:
+            print("😿 Hangman Status:")
+            for i in hangman:
+                print(i)       
+        if drawing == []:
+            print("HANGED THE MAN💀")
+            break
+    else:
+        print('You saved the hanging man🙏')
+
+game_run()
